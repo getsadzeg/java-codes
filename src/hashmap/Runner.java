5521a9edc85a624518f0c68c8ffdf713b2e67dd0
getsadzeg <getsadzeguram@gmail.com>
2016-01-19 22:54:56 +0400

    add hashmap's tiny example

---
+++ src/hashmap/Runner.java
@@ -0,0 +1,11 @@
+package hashmap;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Iterator;
+import java.util.Set;
+public class Runner {
+	public static void main(String[] args) {
+		HashMap<String, String> hmap = new HashMap<String, String>();
+		hmap.put("hello", "გამარჯობა");
+	}
+}
