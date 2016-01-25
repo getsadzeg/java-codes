49dc99b4fbbfaf50f81b75683b1b09316e57c844
getsadzeg <getsadzeguram@gmail.com>
2016-01-25 13:31:09 +0800

    add ArrayList tiny example

---
+++ src/collections/arraylist/Runner.java
@@ -0,0 +1,13 @@
+
+package collections.arraylist;
+import java.util.ArrayList;
+
+public class Runner {
+    public static void main(String[] args) {
+        ArrayList arr = new ArrayList<Integer>();
+        arr.add(10);
+        arr.add(14);
+        arr.add(2);
+        System.out.println(arr);
+    }
+}
