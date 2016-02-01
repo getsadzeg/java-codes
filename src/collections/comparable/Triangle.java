e0b25c850b7223f8b532760283b36bb090a9bfae
getsadzeg <getsadzeguram@gmail.com>
2016-02-01 13:50:51 +0800

    add 'comparable' package; demonstration of 'compareTo' which compares objects

---
+++ src/collections/comparable/Triangle.java
@@ -0,0 +1,56 @@
+
+package comparable;
+
+
+public class Triangle implements Comparable<Triangle>{
+    private double a;
+    private double b;
+    private double c;
+    public Triangle() {
+        
+    }
+    public Triangle(double a, double b, double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public double getA() {
+        return a;
+    }
+
+    public void setA(double a) {
+        this.a = a;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public void setB(double b) {
+        this.b = b;
+    }
+
+    public double getC() {
+        return c;
+    }
+
+    public void setC(double c) {
+        this.c = c;
+    }
+    public double area() {
+        double p = (a+b+c)/2;
+        return Math.sqrt(p*(p-a) * (p-b) * (p-c));
+    }
+    @Override
+    public int compareTo(Triangle o) {
+        if(o.area() > this.area()) {
+            return 1;
+        }
+        else if(o.area() == this.area()) {
+            return 0;
+        }
+        else return -1;
+    }
+    
+}
