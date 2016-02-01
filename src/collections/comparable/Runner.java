cdc1037adc2656b3076c469027b7e984c5a7c3cf
getsadzeg <getsadzeguram@gmail.com>
2016-02-01 14:02:05 +0800

    create ArrayList of Triangle objects and sort them

---
+++ src/collections/comparable/Runner.java
@@ -0,0 +1,17 @@
+
+package comparable;
+import java.util.*;
+        
+public class Runner {
+    public static void main(String[] args) {
+        ArrayList al = new ArrayList<Triangle>();
+        al.add(new Triangle(3,3,3));
+        al.add(new Triangle(1,1,1));
+        al.add(new Triangle(2,2,2));
+        al.add(new Triangle(1,3,3));
+        al.add(new Triangle(1,2,2));
+        System.out.println(al);
+        Collections.sort(al);
+        System.out.println(al);
+    }
+}
