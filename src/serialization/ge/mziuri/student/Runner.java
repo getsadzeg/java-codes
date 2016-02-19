3a6c08781f4148d7e22c2e26b85203fb87ce1bbf
getsadzeg <getsadzeguram@gmail.com>
2016-02-19 13:37:28 +0800

    add reading from file - deserialization

--- src/serialization/ge/mziuri/student/Runner.java
+++ src/serialization/ge/mziuri/student/Runner.java
@@ -5,10 +5,21 @@ import java.io.*;
 public class Runner {
     public static void main(String[] args) {
         Student s = new Student("whoever", 20);
-        try(ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("data.txt"))) {
+        try(ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("data.txt"))) { //writing
             out.writeObject(s);
         }
         catch(IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+		//independent from each other
+        try(ObjectInputStream in = new ObjectInputStream(new FileInputStream("data.txt"))) { //reading
+            Student st = (Student)in.readObject();
+            System.out.println(st.getName() + " " + st.getAge());
+        }
+        catch(IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+        catch(ClassNotFoundException ex) {
             System.out.println(ex.getMessage());
         }
     }
