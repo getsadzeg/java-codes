c1a48a478730ef63b7b1a19d8330e1e049354bbd
getsadzeg <getsadzeguram@gmail.com>
2016-03-28 14:19:50 +1200

    binding to student class

--- src/fx/TableView/NewFXMain.java
+++ src/fx/TableView/NewFXMain.java
@@ -2,6 +2,8 @@
 package TableView;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
@@ -11,6 +13,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
@@ -35,14 +38,17 @@ public class NewFXMain extends Application {
         TableColumn<Student, String> nameColumn = new TableColumn<>("სახელი");
         nameColumn.setMinWidth(200);
         nameColumn.setStyle("-fx-font-family:sylfaen");
+        nameColumn.setCellValueFactory(new PropertyValueFactory<>("Name"));
         TableColumn<Student, String> surnameColumn = new TableColumn<>("გვარი");
         surnameColumn.setMinWidth(200);
         surnameColumn.setStyle("-fx-font-family:sylfaen");
+        surnameColumn.setCellValueFactory(new PropertyValueFactory<>("Surname"));
         TableColumn<Student, Integer> markColumn = new TableColumn<>("ქულა");
         markColumn.setMinWidth(150);
         markColumn.setStyle("-fx-font-family:sylfaen");
+        markColumn.setCellValueFactory(new PropertyValueFactory<>("mark"));
         table.getColumns().addAll(nameColumn, surnameColumn, markColumn);
-        
+        table.setItems(getStudents());
         root.getChildren().addAll(label, table);
         Scene scene = new Scene(root);
         
@@ -51,7 +57,13 @@ public class NewFXMain extends Application {
         primaryStage.show();
     }
 
-    
+    public ObservableList<Student> getStudents() {
+        ObservableList<Student> students = FXCollections.observableArrayList();
+        students.add(new Student("ილია", "ჭავჭავაძე", 9));
+        students.add(new Student("აკაკი", "წერეთელს", 8));
+        students.add(new Student("ლუკა", "რაზიკაშვილი", 10));
+        return students;
+    }
     public static void main(String[] args) {
         launch(args);
     }
