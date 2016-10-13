2ac50bb515cb2b9be4a3c5fa37c214c811657723
getsadzeg <getsadzeguram@gmail.com>
2016-10-13 18:23:14 +0400

    selecting; using poor ResultSet

--- src/ge/mziuri/jdbc/Runner.java
+++ src/ge/mziuri/jdbc/Runner.java
@@ -3,6 +3,7 @@ package ge.mziuri.jdbc;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.Statement;
 
 
@@ -14,6 +15,13 @@ public class Runner {
             Statement st = conn.createStatement();
             st.executeUpdate("CREATE TABLE country ( id INT PRIMARY KEY, name VARCHAR(30))");
             st.executeUpdate("INSERT INTO country VALUES( 1, 'საქართველო')");
+            st.executeUpdate("INSERT INTO country VALUES( 2, 'ინგლისი')");
+            ResultSet rs = st.executeQuery("SELECT * FROM country");
+            while(rs.next()) {
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                System.out.println(id + " " + name);
+            }
         }
         catch(Exception ex) {
             System.out.println(ex.getMessage());
